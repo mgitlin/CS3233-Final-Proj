d6ba469224d9fb91e74a00020db1416ce084ed24
unknown <ifeLawal>
2015-12-02 11:57:11 -0500

    spinners rotate

---
+++ Assets/Scripts/Spinners.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinners : MonoBehaviour {
+	public float spinRate = 1080f;
+	Quaternion originalLocalRotation;
+
+	// Use this for initialization
+	void Start () {
+		originalLocalRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localRotation = Quaternion.Euler(0f, spinRate * Time.time, 0f);
+	}
+}
