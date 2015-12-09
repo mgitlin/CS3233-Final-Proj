00ddcfdc7414e861d8795f058c8ab3f9d002d236
Peter <pakp122134@gmail.com>
2015-12-09 13:55:28 -0500

    made changes to the scene and the cube rotate code

--- Assets/Scripts/cubeRotate.cs
+++ Assets/Scripts/cubeRotate.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class cubeRotate : MonoBehaviour {
 
 	// Use this for initialization
+	public float rotationSpeed;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(0, 0, 20*Time.deltaTime);
+		transform.Rotate(0, 0, rotationSpeed*Time.deltaTime);
 	}
 	void OnCollisionEnter(Collision col) {
 		if (col.gameObject.name == "Drone") {
